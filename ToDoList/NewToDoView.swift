fe507e77173c056a5880abf96cf0802dd207f45c
Yashvi <yutrivedi@gmail.com>
2025-08-08 13:10:16 -0400

    added first two views

---
+++ ToDoList/NewToDoView.swift
@@ -0,0 +1,40 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        VStack {
+            Text("Task title:")
+                .font(.title)
+                .fontWeight(.bold)
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                  .padding()
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+            }
+            .padding()
+            
+            Button {
+
+            } label: {
+                Text("Save")
+                    .bold()
+                    .background(Color(red: 0.949, green: 0.949, blue: 0.971))
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
